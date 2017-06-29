7c54d94e1c24df7e072872710235c16446c395fe
vien1994 <vien1994@yahoo.com>
2017-06-29 13:53:54 -0700

    Delete sample2.js

--- sample2.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');  
-var yelp = require('./yelpMod');
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write("Result: " + yelp.result());
-	res.end(' ... YAY!');
-}).listen(8080);
